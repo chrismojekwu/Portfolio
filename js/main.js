be510ad7b8ea4af52fbb3439804252acbe1d9aed
Chris Mojekwu <chrismojekwu@gmail.com>
2020-10-12 19:06:54 -0500

    added styling for project titles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-$('.proj-button').on('click', e=> {
-   $('.projects-main').toggle('slow', function(){
-   })
+$(".proj-button").on("click", (e) => {
+  $(".projects-main").toggle("slow", function () {});
 });
 
-$("#popup").click()
-
+$("#popup").click();
